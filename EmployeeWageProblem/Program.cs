c1ba68fd3d8244994debb10516373a738904c446
manojpawane <manojpawane@gmail.com>
2020-09-28 09:30:09 +0530

    [Manoj] Add . Employee Present or Absent code added

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int FULL_TIME = 1;
+            int empWagePerHour = 20;
+            int empHours = 0;
+            int totalWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if(empCheck == FULL_TIME)
+            {
+                empHours = 8;
+            }
+            else
+            {
+                empHours = 0;
+            }
+            totalWage = empHours * empWagePerHour;
+            Console.WriteLine("Total Wage.. "+totalWage);
+            Console.ReadKey();
+        }
+    }
+}
